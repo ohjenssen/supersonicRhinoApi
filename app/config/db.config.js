8bbae3fbaaf2189915a97ce15ec8eb41ef5fc6a2
Oskar Heming Jenssen <oss@watts.dk>
2025-12-04 20:15:35 +0100

    db update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: process.env.MYSQLHOST || process.env.DB_HOST || "localhost",
-    USER: process.env.MYSQLUSER || process.env.DB_USER || "root",
-    PASSWORD: process.env.MYSQLPASSSWORD || process.env.DB_PASSWORD || "root_root#69",
-    DB: process.env.MYSQLDATABASE || process.env.DB_NAME || 'supersonicrhinoDB',
+    HOST: process.env.MYSQLHOST || process.env.DB_HOST,
+    USER: process.env.MYSQLUSER || process.env.DB_USER,
+    PASSWORD: process.env.MYSQLPASSSWORD || process.env.DB_PASSWORD,
+    DB: process.env.MYSQLDATABASE || process.env.DB_NAME,
     dialect: "mysql",
     pool: {
         max: 5, 
